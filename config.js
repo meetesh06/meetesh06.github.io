ab0718b780b00bd32da4832fa416c09245c278f5
meetesh06 <meeteshmehta4@gmail.com>
2024-02-09 22:28:08 +0530

    with google tag

--- config.js
+++ config.js
@@ -40,10 +40,5 @@ export const TITLE_BLOG = "Meetesh Mehta's Blog"
 export const DESCRIPTION_BLOG = "Welcome to my personal blog, where I document my various interests and activities! As a graduate student at IIT Mandi, I am constantly engaged in research and academic pursuits, particularly in the field of compilers. I am always excited to share my latest findings and insights with fellow enthusiasts in this area. In addition to my academic pursuits, I also enjoy exploring the world of computer programming, with a particular focus on the React library. As an avid coder, I enjoy using this platform to create engaging and dynamic websites that showcase my skills and abilities."
 
 export const GTAG_ADDR = "https://www.googletagmanager.com/gtag/js?id=G-6Z2X65E38W"
-export const GTAG_SCRIPT = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
-gtag('config', 'G-6Z2X65E38W');
-`
+// Google tag script is in public/google_tag_script.js
